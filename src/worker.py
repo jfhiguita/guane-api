e9c4ae5533f92e1ff28921cb00f9702d97c419ed
John Higuita <johnfrehiguita@gmail.com>
2021-06-11 09:17:21 -0500

    set up celery

--- src/worker.py
+++ src/worker.py
@@ -11,8 +11,10 @@ from celery.utils.log import get_task_logger
 
 # init celery
 celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@fastapi-rabbit:5672")
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://:password123@redis:6379/0")
+celery.conf.task_routes = {"worker.celery.test_celery": "test-queue"}
+celery.conf.update(task_track_started=True)
 
 # create logger
 logger = get_task_logger(__name__)
